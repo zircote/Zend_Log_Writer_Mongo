9ee7d2cf11e7eead0cd0f76aed398eac493c7f29
Robert Allen <zircote@gmail.com>
2012-03-15 08:46:13 -0500

    added priority and class documentation

--- library/Zend/Tool/Project/Provider/MongoLog.php
+++ library/Zend/Tool/Project/Provider/MongoLog.php
@@ -1,22 +1,20 @@
 <?php
 /**
  *
- * @category   Zend
+ * @category   Ifbyphone
  * @package    Zend_Tool
  * @subpackage Framework
  */
 /**
  * @see Zend_Tool_Project_Provider_AbstractProvider
  */
-require_once dirname(__DIR__) . '/library/Zend/Tool/Project/Provider/AbstractProvider.php';
+require_once 'Zend/Tool/Project/Provider/Abstract.php';
 /**
  *
  *
- * @category   Zend
+ * @category   Ifbyphone
  * @package    Zend_Tool
  * @subpackage Framework
- *
- *
  */
 class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abstract
 {
@@ -36,7 +34,6 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
      */
     protected $_mongoCollection;
     /**
-     * (non-PHPdoc)
      * @see Zend_Tool_Project_Provider_AbstractProvider::initialize()
      */
     public function initialize()
@@ -45,11 +42,7 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
             Zend_Cache::throwException("Cannot use Mongo storage because the ".
             "'Mongo' extension is not loaded in the current PHP environment");
         }
-        if (!self::$_isInitialized) {
-            parent::initialize();
-            $this->_loadConfig();
-            $this->_setMongo();
-        }
+        parent::initialize();
     }
 
     /**
@@ -57,14 +50,15 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
      * @return Zend_Tool_Project_Provider_MongoLog
      * @throws Zend_Tool_Project_Exception
      */
-    protected function _loadConfig()
+    protected function _loadConfig($env)
     {
         $profile = $this->_loadProfile(self::NO_PROFILE_THROW_EXCEPTION);
         $appConfigFileResource = $profile->search('applicationConfigFile');
         $appConfigFilePath = $appConfigFileResource->getPath();
         $this->_setConfig(
-            new Zend_Config_Ini($appConfigFilePath, APPLICATION_ENV)
+            new Zend_Config_Ini($appConfigFilePath, $env)
         );
+        $this->_setMongo();
         return $this;
     }
     /**
@@ -79,9 +73,9 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
         }
         $this->_config = $config;
         foreach ($this->_config['resources']['log'] as $log) {
-            if(strstr($log['writerParams']['writerName'], 'MongoDb')){
-                $this->_mongoParams = $log['writerParams']['writerName'];
-                $this->_documentMap = $log['documentMap'];
+            if(isset($log['writerName']) && strstr($log['writerName'], 'MongoDb')){
+                $this->_mongoParams = $log['writerParams'];
+                $this->_documentMap = $log['writerParams']['documentMap'];
                 return $this;
             }
         }
@@ -93,16 +87,17 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
      * <b>Example Usage:</b>
      *
      * <code>
-     * zf tail log -h 'some-host.cloud.com' -f 'somebad thing happened' -e production
+     * zf tail log -h 'some-host.cloud.com' -p 7 -f 'somebad thing happened' -e production
      * </code>
      *
      * @param string $filter text to filter the logs by
-     * @param server $hostname comma seperated lists of servers to return logs for
-     * @param int $priority The log priority to filter the tail on
+     * @param string $hostname a comma seperated lists of servers to return logs for
+     * @param int    $priority The log priority to filter the tail on
      * @param string $env project environment by which the configs are loaded
      */
-    public function tail($filter = null, $hostname = null, $priority = null, $env = 'development')
+    public function tail($filter = null, $hostname = null, $priority = null, $env = 'dev')
     {
+        $this->_loadConfig($env);
         $_f = array();
         if($filter){
             $_f['message'] = array('$regex' => sprintf('%s', $filter));
@@ -156,7 +151,7 @@ class Zend_Tool_Project_Provider_MongoLog extends Zend_Tool_Project_Provider_Abs
      * @param array $config
      * @return MongoCollection
      */
-    protected function _createMongoCollection($config)
+    protected function _createMongoCollection()
     {
         if(!isset($this->_mongoParams['server'])){
             $server  = "mongodb://localhost:27017";
